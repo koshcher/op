aebc94b74b993653b875346686928eeb358dc3a5
Roman Koshchei <romankoshchei@gmail.com>
2023-09-24 20:05:34 +0300

    mermaid can be used with straight lines

--- site/labs/1.data.js
+++ site/labs/1.data.js
@@ -6,3 +6,24 @@ export default {
     return code
   },
 }
+
+/*
+<script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>
+<div class="mermaid">
+
+%%{init: {'flowchart' : {'curve' : 'stepAfter'}}}%%
+
+flowchart  TD
+  A((Start))
+  A --- B
+  A --- C
+  A --- D
+  A --- E
+  B --- B1
+  B --- B2
+
+  C --- B1
+
+</div>
+
+*/
