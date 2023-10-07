816108107a9bced661bd31be37d8bd845c74d37a
Roman Koshchei <romankoshchei@gmail.com>
2023-10-07 21:47:39 +0300

    lab 5 1 task

--- src/labs/lab5.h
+++ src/labs/lab5.h
@@ -5,54 +5,77 @@
 #include "shared.h"
 
 namespace lab5 {
+struct CountResult {
+    unsigned long operations;
+    std::optional<unsigned long long> count;
+};
+
 /// <summary>
 /// C = total! / (take! * (total - take)!)
 /// </summary>
-std::optional<unsigned long long> countSituationsLoop(const long& total, const long& take) {
+CountResult countSituationsLoop(const long& total, const long& take) {
     unsigned long long totalFactorial = 1;
     unsigned long long takeFactorial = 1;
     unsigned long long totalMinusTakeFactorial = 1;
     const long totalMinusTake = total - take;
 
-    for (long i = 2; i <= total; i += 1) {
+    unsigned long iteration = 2;
+    for (; iteration <= total; iteration += 1) {
         // Check for overflow of result
-        if (totalFactorial > ULLONG_MAX / i) return std::nullopt;
-        totalFactorial *= i;
+        if (totalFactorial > ULLONG_MAX / iteration) return { iteration - 1, std::nullopt };
+        totalFactorial *= iteration;
 
-        if (i <= take) takeFactorial = totalFactorial;
-        if (i <= totalMinusTake) totalMinusTakeFactorial = totalFactorial;
+        if (iteration <= take) takeFactorial = totalFactorial;
+        if (iteration <= totalMinusTake) totalMinusTakeFactorial = totalFactorial;
     }
 
-    return totalFactorial / (takeFactorial * totalMinusTakeFactorial);
+    return { iteration - 2,  totalFactorial / (takeFactorial * totalMinusTakeFactorial) };
 }
 
-std::optional<unsigned long long> calculateFactorialRecursively(const long& num) {
-    if (num <= 1) return 1;
+CountResult calculateFactorialRecursively(const long& num) {
+    if (num <= 1) return { 1, 1 };
 
     const auto prev = calculateFactorialRecursively(num - 1);
-    if (!prev.has_value()) return std::nullopt;
-    if (num > ULLONG_MAX / prev.value()) return std::nullopt;
+    if (!prev.count.has_value() || num > ULLONG_MAX / prev.count.value()) {
+        return { prev.operations, std::nullopt };
+    }
 
-    return num * prev.value();
+    return { prev.operations + 1, num * prev.count.value() };
 }
 
-std::optional<unsigned long long> countSituationsRecursive(const long& total, const long& take) {
+CountResult countSituationsRecursive(const long& total, const long& take) {
     try {
-        const auto totalFactorial = calculateFactorialRecursively(total);
-        if (!totalFactorial.has_value()) return std::nullopt;
-        const auto takeFactorial = calculateFactorialRecursively(take);
-        if (!takeFactorial.has_value()) return std::nullopt;
-        const auto totalMinusTakeFactorial = calculateFactorialRecursively(total - take);
-        if (!totalMinusTakeFactorial.has_value()) return std::nullopt;
+        unsigned long operations = 0;
 
-        return totalFactorial.value() / (takeFactorial.value() * totalMinusTakeFactorial.value());
+        const auto totalFactorial = calculateFactorialRecursively(total);
+        operations += totalFactorial.operations;
+        if (!totalFactorial.count.has_value()) {
+            return { operations, std::nullopt };
+        }
+
+        const auto takeFactorial = calculateFactorialRecursively(take);
+        operations += takeFactorial.operations;
+        if (!takeFactorial.count.has_value()) {
+            return { operations, std::nullopt };
+        }
+
+        const auto totalMinusTakeFactorial = calculateFactorialRecursively(total - take);
+        operations += totalMinusTakeFactorial.operations;
+        if (!totalMinusTakeFactorial.count.has_value()) {
+            return { operations, std::nullopt };
+        }
+
+        return {
+            operations,
+            totalFactorial.count.value() / (takeFactorial.count.value() * totalMinusTakeFactorial.count.value())
+        };
     }
     catch (...) {
-        return std::nullopt;
+        return { 0, std::nullopt };
     }
 }
 
-using CountSituationsStrategy = std::function<std::optional<unsigned long long>(const long&, const long&)>;
+using CountSituationsStrategy = std::function<CountResult(const long&, const long&)>;
 
 void countSituations(
     const CountSituationsStrategy& strategy,
@@ -65,7 +88,7 @@ void countSituations(
 
     // only 2 winning
     const auto winningSituations = strategy(winningCount, 2);
-    if (!winningSituations.has_value()) {
+    if (!winningSituations.count.has_value()) {
         std::cout << "Number is too big to calculate with standard functionality" << std::endl;
         return;
     }
@@ -74,32 +97,34 @@ void countSituations(
     }
     else {
         const auto loosingSituations = strategy(loosingCount, ticketsToTakeCount - 2);
-        if (!loosingSituations.has_value()) {
+        if (!loosingSituations.count.has_value()) {
             std::cout << "Number is too big to calculate with standard functionality" << std::endl;
             return;
         }
 
-        if (ULLONG_MAX / loosingSituations.value() < winningSituations.value()) {
+        if (ULLONG_MAX / loosingSituations.count.value() < winningSituations.count.value()) {
             std::cout << "Number is too big to calculate with standard functionality" << std::endl;
             return;
         }
-        const unsigned long long only2Winning = winningSituations.value() * loosingSituations.value();
+        const unsigned long long only2Winning = winningSituations.count.value() * loosingSituations.count.value();
         std::cout << "Situations when only 2 tickets are winning: " << only2Winning << std::endl;
+        std::cout << "Operations count: " << winningSituations.operations + loosingSituations.operations << std::endl;
     }
 
     // at least 2 winning
     const auto takingSituations = strategy(totalTicketsCount - 2, ticketsToTakeCount - 2);
-    if (!takingSituations.has_value()) {
+    if (!takingSituations.count.has_value()) {
         std::cout << "Number is too big to calculate with standard functionality" << std::endl;
         return;
     }
 
-    if (ULLONG_MAX / takingSituations.value() < winningSituations.value()) {
+    if (ULLONG_MAX / takingSituations.count.value() < winningSituations.count.value()) {
         std::cout << "Number is too big to calculate with standard functionality" << std::endl;
         return;
     }
-    const unsigned long long atLeast2Winning = winningSituations.value() * takingSituations.value();
+    const unsigned long long atLeast2Winning = winningSituations.count.value() * takingSituations.count.value();
     std::cout << "Situations when at least 2 tickets are winning: " << atLeast2Winning << std::endl;
+    std::cout << "Operations count: " << winningSituations.operations + takingSituations.operations << std::endl;
 }
 
 void task1() {
