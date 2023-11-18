a51c919091108c0f74fc42426a812452aa6acd3b
Roman Koshchei <romankoshchei@gmail.com>
2023-11-18 20:58:54 +0200

    lab 8

--- src/labs/main.cpp
+++ src/labs/main.cpp
@@ -5,6 +5,7 @@
 #include "lab5.h"
 #include "lab6.h"
 #include "lab7.h"
+#include "lab8.h"
 #include "shared.h"
 #include <iostream>
 
@@ -15,7 +16,7 @@ int main() {
         shared::select({
             { "Lab 1", lab1::run }, { "Lab 2", lab2::run }, { "Lab 3", lab3::run },
             { "Lab 4", lab4::run }, { "Lab 5", lab5::run }, { "Lab 6", lab6::run },
-            { "Lab 7", lab7::run }
+            { "Lab 7", lab7::run }, { "Lab 8", lab8::run }
         });
     }
     catch (...) {
